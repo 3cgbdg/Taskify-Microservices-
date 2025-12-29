6374dc91027de4cf401fb5a2a0934ae95b983080
gaykun1 <139235847+gaykun1@users.noreply.github.com>
2025-12-29 23:00:04 +0200

    (auth-service): finished

--- microservices/apps/api-gateway/src/auth/auth.controller.ts
+++ microservices/apps/api-gateway/src/auth/auth.controller.ts
@@ -1,21 +1,38 @@
-import { Body, Controller, Get, Inject, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { MICROSERVICES_CLIENTS } from 'src/constants';
 import { ClientProxy } from '@nestjs/microservices';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { LoginAuthDto } from './dto/login-auth.dto';
+import { firstValueFrom } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
+import type { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-    constructor(@Inject(MICROSERVICES_CLIENTS.AUTH_SERVICE) private readonly authClient: ClientProxy) { }
+    constructor(@Inject(MICROSERVICES_CLIENTS.AUTH_SERVICE) private readonly authClient: ClientProxy, private readonly http: HttpService) { }
     @Post("signup")
-    async signUp(@Body() dto: CreateAuthDto) {
-        return this.authClient.send("auth.signup", dto);
+    async signUp(@Res() res: Response, @Body() dto: CreateAuthDto) {
+        const response = await firstValueFrom(
+            this.http.post('http://auth-service:3001/auth/signup', dto, {
+                withCredentials: true,
+            }),
+        );
+
+        res.setHeader('set-cookie', response.headers['set-cookie']!);
+        return res.json(response.data);
     }
 
     @Post("login")
-    async logIn(@Body() dto: LoginAuthDto) {
-        return this.authClient.send("auth.login", dto);
+    async logIn(@Res() res: Response, @Body() dto: LoginAuthDto) {
+        const response = await firstValueFrom(
+            this.http.post('http://auth-service:3001/auth/login', dto, {
+                withCredentials: true,
+            }),
+        );
+
+        res.setHeader('set-cookie', response.headers['set-cookie']!);
+        return res.json(response.data);
     }
 
     @UseGuards(AuthGuard("jwt"))
@@ -23,6 +40,31 @@ export class AuthController {
     async getOwnProfile(@Req() req: Request) {
         const id = (req as any).user.id;
         return this.authClient.send("auth.profile", id);
+    }
+
+    @Delete("logout")
+    async logOut(@Res() res: Response) {
+        const response = await firstValueFrom(
+            this.http.delete('http://auth-service:3001/auth/logout', {
+                withCredentials: true,
+            }),
+        );
+        res.setHeader('set-cookie', response.headers['set-cookie']!);
+        return res.json(response.data);
+    }
+    @UseGuards(AuthGuard("jwt"))
+    @Post("refresh")
+    async refresh(@Res() res: Response) {
+        const response = await firstValueFrom(
+            this.http.post('http://auth-service:3001/auth/refresh', {
+                withCredentials: true,
+            }),
+        );
+        res.setHeader('set-cookie', response.headers['set-cookie']!);
+        return res.json(response.data);
 
     }
+
+
+
 }
